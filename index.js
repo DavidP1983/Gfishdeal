efa219843db4211882145a5347c045e745c03936
David <David.Piruzashvili@gmail.com>
2024-09-07 19:47:46 +0400

    changes

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const btn = document.getElementById('btn');
 const spinner = document.getElementById('spinner');
-const toast = document.getElementById('liveToastBtn');
 const popup = document.getElementById('liveToast');
 
 function show() {
@@ -10,7 +9,7 @@ function show() {
 }
 
 function hide() {
-    const timer = setTimeout(hide, 2000);
+    const timer = setTimeout(hide, 3000);
     function hide() {
         popup.classList.remove('show');
         popup.classList.add('hide');
@@ -23,12 +22,32 @@ document.getElementById('form').addEventListener('submit', function (event) {
     event.preventDefault();
 
     const formData = new FormData(this);
+
+    function createPhoneNumber(numbers) {
+        const arr = numbers.split(',');
+        const [fr, th, tr, ft] = arr.join('').match(/(\d{3})|(\d+)/g);
+        return `+(${fr}) ${th}-${tr}-${ft}`
+    }
+
+
+    const obj = {};
+    formData.forEach((val, key) => {
+        if (key === 'thelepone') {
+            obj[key] = createPhoneNumber(val);
+        } else {
+            obj[key] = val;
+        }
+
+    })
+
     btn.setAttribute('disabled', '');
     spinner.style.display = 'block';
 
-    fetch('server.php', {
+
+    fetch('/', {
         method: 'POST',
-        body: formData,
+        headers: { 'Content-type': "application/json" },
+        body: JSON.stringify(obj),
     })
         .then(response => response.text())
         .then(result => {
@@ -36,9 +55,13 @@ document.getElementById('form').addEventListener('submit', function (event) {
             spinner.style.display = 'none';
             show();
         })
-        .catch(error => {
-            console.error('Ошибка:', error);
-            document.getElementById('message').innerHTML = 'Error please try again';
+        .catch((error) => {
+            console.log('Ошибка:', error);
+            spinner.style.display = 'none';
+            btn.removeAttribute('disabled', '');
+            document.querySelector('#liveToast #message').style.color = 'red';
+            document.querySelector('#liveToast #message').innerHTML = 'Error please try again';
+            show();
         })
         .finally(() => {
             document.getElementById('form').reset();
